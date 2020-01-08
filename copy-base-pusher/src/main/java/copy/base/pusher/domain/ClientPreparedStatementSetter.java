bb6ff0d879570d3ba9116f5592d302facf1e247b
Karol Mendza <karolmendza@gmail.com>
2020-01-08 23:42:25 +0100

    Refactor

--- copy-base-pusher/src/main/java/copy/base/pusher/domain/ClientPreparedStatementSetter.java
+++ copy-base-pusher/src/main/java/copy/base/pusher/domain/ClientPreparedStatementSetter.java
@@ -7,12 +7,18 @@ import java.sql.SQLException;
 
 public class ClientPreparedStatementSetter implements ItemPreparedStatementSetter<Client> {
 
+    public static final int ID_INDEX = 1;
+    public static final int FIRST_NAME_INDEX = 2;
+    public static final int LAST_NAME_INDEX = 3;
+    public static final int EMAIL_INDEX = 4;
+    public static final int PHONE_INDEX = 5;
+
     @Override
     public void setValues(Client item, PreparedStatement ps) throws SQLException {
-        ps.setLong(1, item.getId());
-        ps.setString(2, item.getFirstName());
-        ps.setString(3, item.getLastName());
-        ps.setString(4, item.getEmail());
-        ps.setString(5, item.getPhone());
+        ps.setLong(ID_INDEX, item.getId());
+        ps.setString(FIRST_NAME_INDEX, item.getFirstName());
+        ps.setString(LAST_NAME_INDEX, item.getLastName());
+        ps.setString(EMAIL_INDEX, item.getEmail());
+        ps.setString(PHONE_INDEX, item.getPhone());
     }
 }
