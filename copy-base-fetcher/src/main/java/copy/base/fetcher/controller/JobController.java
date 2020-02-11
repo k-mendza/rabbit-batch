bf064e2aae3f01066938097e1506650a9bec22b6
Karol Mendza <karolmendza@gmail.com>
2020-02-11 23:59:36 +0100

    Initial work on job triggered by endpoint

--- copy-base-fetcher/src/main/java/copy/base/fetcher/controller/JobController.java
+++ copy-base-fetcher/src/main/java/copy/base/fetcher/controller/JobController.java
@@ -1,6 +1,11 @@
 package copy.base.fetcher.controller;
 
 
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,8 +16,18 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequestMapping("/api/v1/job")
 public class JobController {
 
+    private final JobLauncher jobLauncher;
+    private final Job job;
+
+    public JobController(JobLauncher jobLauncher, Job job) {
+        this.jobLauncher = jobLauncher;
+        this.job = job;
+    }
+
     @RequestMapping("/{authKey}")
-    public @ResponseBody int startJob(@PathVariable(value="authKey") String authKey){
+    public @ResponseBody int startJob(@PathVariable(value="authKey") String authKey) throws Exception {
+        JobParameters jobParameters = new JobParametersBuilder().toJobParameters();
+        final JobExecution jobExecution = jobLauncher.run(job, jobParameters);
         return authKey.equals("abcd") ? HttpStatus.OK.value() : HttpStatus.UNAUTHORIZED.value();
     }
 }
